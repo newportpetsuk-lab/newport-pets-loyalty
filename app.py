0bfa889de399563d3c65bd51e7aaa56c35ba6454
newportpetsuk-lab <newportpetsuk@gmail.com>
2026-03-22 06:50:00 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, session, redirect
+from flask import Flask, render_template, request, session, redirect, url_for
 import sqlite3
 import qrcode
 import os
@@ -516,5 +516,8 @@ def history(customer_id):
 # RUN
 # -------------------------
 
+import os
+
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
