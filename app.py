3f12022f0abfb5b4c4d840529b66eac4ad45802b
newportpetsuk-lab <newportpetsuk@gmail.com>
2026-03-23 11:17:55 +0000

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("welcome.html")
+    return "FINAL IS WORKING ✅"
